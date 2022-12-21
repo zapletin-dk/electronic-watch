49ac49dee61f56ebe6daca0ee3789a9f20ef6bc3
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-21 22:09:28 +0300

    Pretty easy task but in my opinion format of output time should be 00:00:00

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,14 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
-
+        int hours;
+        int minutes;
+        int sec;
+        hours = seconds/3600 ;
+        minutes = (seconds - hours*3600)/60;
+        sec = (seconds - hours*3600 - minutes*60);
+        int formattedHours = hours >=24 ? hours -24 : hours;
+        String result = String.format("%d:%02d:%02d", formattedHours, minutes, sec);
+        System.out.println(result);
     }
 }
