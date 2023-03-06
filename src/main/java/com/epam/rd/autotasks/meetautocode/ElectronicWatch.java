2b3aa49af9eddc2bbbc452610fbfbaf20595694e
Zapletin Daniil <Zaplot1994@gmail.com>
2023-03-06 20:29:45 +0300

    Fix for cases when seconds are more than 10000

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
-        int hours;
-        int minutes;
-        int sec;
-        hours = seconds/3600 ;
-        minutes = (seconds - hours*3600)/60;
-        sec = (seconds - hours*3600 - minutes*60);
-        int formattedHours = hours >=24 ? hours -24 : hours;
-        String result = String.format("%d:%02d:%02d", formattedHours, minutes, sec);
-        System.out.println(result);
+        int hours, minutes, seconds = scanner.nextInt();
+
+        hours = seconds/(60 * 60) ;
+        minutes = (seconds - hours * (60 * 60)) / 60;
+        seconds = (seconds - hours * (60 * 60) - minutes * 60);
+        int formattedHours = hours >= 24 ? hours -hours/24 * 24 : hours;
+
+        System.out.printf("%d:%02d:%02d%n", formattedHours, minutes, seconds);
     }
 }
